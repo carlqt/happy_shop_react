e8dad2029ebf755de8b2b0e4252221313396d004
Carl Tablante <carl.q.tablante@gmail.com>
2019-03-10 17:40:40 +0800

    Added sort by select component

--- src/App/Products/filterSection.js
+++ src/App/Products/filterSection.js
@@ -7,93 +7,18 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import { withStyles } from '@material-ui/core/styles';
-import { debounce } from 'throttle-debounce';
-import produce from 'immer';
-import isEqual from 'lodash.isequal';
 import { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 
 class FilterSection extends Component {
-  constructor() {
-    super();
-    this.state = {
-      categories: [],
-      priceRange: [0, 30000]
-    }
-  }
-
-  componentDidUpdate = (prevProps, prevState) => {
-    const { categories } = this.state;
-
-    if (!isEqual(prevState, this.state)) {
-      this.loadProducts(categories)
-    }
-  }
-
-  loadProducts = debounce(300, (categories) => {
-    const { loadProducts } = this.props;
-    loadProducts(this.formatFilterParams(this.state));
-  })
-
-  isEqual = (prev, current) => {
-    if (prev.length !== current.length) {
-      return false;
-    }
-
-    return prev.every( e => current.includes(e))
-  }
-
-  onFilterSelect = (e) => {
-    if (e.target.checked) {
-      this.addCategory(e.target.value)
-    } else {
-      this.removeCategory(e.target.value)
-    }
-  }
-
-  addCategory = (val) => {
-    this.setState(
-      produce(draft => {
-        draft.categories.push(val)
-      })
-    )
-  }
-
-  removeCategory = (val) => {
-    this.setState(
-      produce(draft => {
-        draft.categories = draft.categories.filter(item => item !== val)
-      })
-    )
-  }
-
-  onSliderChange = (value) => {
-    this.setState(
-      produce(draft => {
-        draft.priceRange = value;
-      })
-    )
-  }
-
-  formatFilterParams = (params) => {
-    const { priceRange, ...otherParams } = params;
-
-    return {
-      ...otherParams,
-      price: `${priceRange[0]}..${priceRange[1]}`,
-    }
-  }
-
-  onAfterChange = (val) => {
-    const { loadProducts } = this.props;
-
-    loadProducts(this.formatFilterParams(this.state));
-  }
-
   render() {
-    const { classes } = this.props;
-    const { priceRange } = this.state;
+    const { 
+      classes,
+      onFilterSelect,
+      queryParams: { priceRange },
+      onSliderChange,
+    } = this.props;
 
     return (
       <Grid className={classes.drawer} item xs={12}>
@@ -105,11 +30,11 @@ class FilterSection extends Component {
           </FormLabel>
           <FormGroup>
             <FormControlLabel
-              control={<Checkbox onChange={this.onFilterSelect} value="makeup" color="primary" />}
+              control={<Checkbox onChange={onFilterSelect} value="makeup" color="primary" />}
               label="Makeup"
             />
             <FormControlLabel
-              control={<Checkbox onChange={this.onFilterSelect} value="organic" color="primary" />}
+              control={<Checkbox onChange={onFilterSelect} value="organic" color="primary" />}
               label="Organic"
             />
 
@@ -126,7 +51,7 @@ class FilterSection extends Component {
                 allowCross={false}
                 value={priceRange}
                 step={1}
-                onChange={this.onSliderChange}
+                onChange={onSliderChange}
               />
               Price: { `$${priceRange[0] / 100} - $${priceRange[1] / 100}` }
             </Typography>
--- src/App/Products/index.js
+++ src/App/Products/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
+import isEqual from 'lodash.isequal';
 
 import { getProducts } from 'Requests'
 import List from './itemList';
 import FilterSection from './filterSection';
-
+import produce from 'immer';
+import { debounce } from 'throttle-debounce';
 
 class ProductList extends Component {
   constructor() {
@@ -13,6 +15,11 @@ class ProductList extends Component {
     this.state = {
       products: [],
       meta: {},
+      queryParams: {
+        sort: '',
+        categories: [],
+        priceRange: [0, 30000]
+      },
     };
   }
 
@@ -20,27 +27,114 @@ class ProductList extends Component {
     this.loadProducts()
   }
 
-  loadProducts = async (params = {}) => {
+  componentDidUpdate = (prevProps, prevState) => {
+    const { queryParams } = this.state;
+
+    if (!isEqual(prevState.queryParams, queryParams)) {
+      this.loadProducts(this.formatFilterParams(queryParams))
+    }
+  }
+
+  loadProducts = debounce(300, (async(params = {}) => {
     const products = await getProducts(params)
-    this.setState({
-      products: products.data,
-      meta: products.meta,
-    });
+    this.setState(
+      produce(draft => {
+        draft.products = products.data
+        draft.meta = products.meta
+      })
+    );
+  }))
+
+  onFilterSelect = (e) => {
+    if (e.target.checked) {
+      this.addCategory(e.target.value)
+    } else {
+      this.removeCategory(e.target.value)
+    }
+  }
+
+  addCategory = (val) => {
+    this.setState(
+      produce(draft => {
+        draft.queryParams.categories.push(val)
+      })
+    )
+  }
+
+  removeCategory = (val) => {
+    this.setState(
+      produce(draft => {
+        draft.queryParams.categories = draft.queryParams.categories.filter(item => item !== val)
+      })
+    )
+  }
+
+  onSliderChange = (value) => {
+    this.setState(
+      produce(draft => {
+        draft.queryParams.priceRange = value;
+      })
+    )
+  }
+
+  formatFilterParams = (params) => {
+    const {
+      priceRange,
+      sort,
+      ...otherParams
+    } = params;
+
+    const obj = {
+      ...otherParams,
+      price: `${priceRange[0]}..${priceRange[1]}`,
+    };
+
+    if (sort) {
+      obj.sort = sort      
+    };
+
+    return obj;
+  }
+
+  handleSelect = (e) => {
+    const { value } = e.target;
+
+    this.setState(
+      produce(draft => {
+        draft.queryParams.sort = value;
+      })
+    )
   }
 
   render() {
-    const { products, meta } = this.state;
+    const {
+      products,
+      meta,
+      queryParams,
+    } = this.state;
     const { classes } = this.props;
 
     return (
       <Grid className={classes.container} container spacing={0}>
         <Grid className={classes.drawerContainer} item xs={3}>
           <FilterSection
-            {...{ meta }}
+            {...{ meta, queryParams }}
+            onFilterSelect={this.onFilterSelect}
+            onSliderChange={this.onSliderChange}
             loadProducts={this.loadProducts}
           />
         </Grid>
         <Grid className={classes.drawer} item xs={9}>
+          <Grid className={classes.drawer} item xs={12}>
+            <label>
+              Sort By: 
+              <select value={queryParams.sort} onChange={this.handleSelect}>
+                <option value=""> -- no sort -- </option>
+                <option value="price">Price (low to high)</option>
+                <option value="-price">Price (high to low)</option>
+              </select>
+            </label>
+          </Grid>
           <List data={products} />
         </Grid>
       </Grid>
@@ -55,8 +149,6 @@ const styles = theme => ({
   drawer: {
     padding: 24,
   },
-  item: {
-  }
 });
 
 export default withStyles(styles)(ProductList);
