7ddf20806d36b9b1d34b6d04e5be6e185cb90ca2
Carl Tablante <carl.q.tablante@gmail.com>
2019-03-12 16:57:14 +0800

    Refactor ProductsList for redux

--- src/App/Products/index.js
+++ src/App/Products/index.js
@@ -1,16 +1,18 @@
 import { connect } from "react-redux";
 
 import { getProducts } from 'Store/Products/actions';
+import { setFilters } from 'Store/QueryParams/actions';
 import Products from './products';
 
 const mapStateToProps = state => ({
   products: state.productStore.products,
   meta: state.productStore.meta,
-  queryParams: state.productStore.queryParams,
+  queryParams: state.queryParamsStore,
 });
 
 const mapDispatchToProps = {
   getProducts,
+  setFilters,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
--- src/Store/index.js
+++ src/Store/index.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, createStore, compose } from "redux";
 import thunk from 'redux-thunk';
 
 import productsReducer from 'Store/Products/reducer';
+import queryParamsReducer from 'Store/QueryParams/reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const combinedReducers = combineReducers({
   productStore: productsReducer,
+  queryParamsStore: queryParamsReducer,
 });
 
 export default createStore(combinedReducers, composeEnhancers(applyMiddleware(thunk)));
